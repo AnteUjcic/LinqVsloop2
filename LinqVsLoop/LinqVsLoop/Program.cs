819e3f2dcfb69fd26390a96aa9b1369941c26197
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-11-28 18:44:40 +0100

    Add files via upload

---
+++ LinqVsLoop/LinqVsLoop/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+namespace LINQvsLoop
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] studentArray = {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+            new Student() { StudentID = 7, StudentName = "Rob",Age = 19  },
+        };
+
+            Student[] students = new Student[10];
+
+            var descent = from stu in studentArray
+                                          where stu.Age >12 && stu.Age<20
+                                          orderby stu.StudentName descending
+                                          select stu;
+            int i = 0;
+
+            foreach (var s in descent)
+            {
+                Console.WriteLine(s.StudentID + " " + s.StudentName + " " + s.Age);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
